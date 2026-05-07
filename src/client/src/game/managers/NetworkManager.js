d14a3f69ecfe454b0f45bfaab51f2ab3d3c96d25
Adiyasa Setia Lesmana <yasaaadi@gmail.com>
2026-05-07 12:58:44 +0700

    Update : NetworkManager

--- src/client/src/game/managers/NetworkManager.js
+++ src/client/src/game/managers/NetworkManager.js
@@ -6,7 +6,7 @@ class NetworkManager {
   }
 
   connect(playerId) {
-    this.ws = new WebSocket('ws://chat-multiplayer-game-pixi.onrender.com');
+    this.ws = new WebSocket('wss://chat-multiplayer-game-pixi.onrender.com');
     
     this.ws.onopen = () => {
       // Send initial join payload (Like OnClientConnect)
